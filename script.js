204ff79b1ed07741d89cee7e79034c0bbc8bf251
Gauri Mhaiskar <gauri.mhaiskar@gmail.com>
2021-05-01 19:22:16 -0400

    added picture in picture and media stream

--- script.js
+++ script.js
@@ -2,3 +2,22 @@
 // console.log("Hi there!");
 const videoEl = document.getElementById("video");
 const buttonEl = document.getElementById("button");
+
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoEl.srcObject = mediaStream;
+    videoEl.onloadedmetadata = () => {
+      videoEl.play();
+    };
+  } catch (err) {
+    console.log(`Whoops, error!`, err);
+  }
+}
+//on load
+buttonEl.addEventListener("click", async () => {
+  buttonEl.disabled = true;
+  await videoEl.requestPictureInPicture();
+  buttonEl.disabled = false;
+});
+selectMediaStream();
